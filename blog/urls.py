d54b1913a643c51a307fef756b044fc361e1d0e0
PilipchukDenis <grutden@mail.ru>
2024-12-11 20:17:17 +0300

    шаблон

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,25 @@
-
-# blog/urls.py (Главная конфигурация URL)
+# blog\urls.py ГЛАВНЫЙ
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from python_blog.views import main
+from django.urls import include
+"""
+Конверторы путей Django:
+str - строки, любые символы кроме слэша '/' (по умолчанию)
+int - положительные целые числа включая 0
+slug - ASCII буквы/цифры, дефисы и подчеркивания
+uuid - уникальные идентификаторы UUID пример '075194d3-6885-417e-a8a8-6c931e272f00'
+path - строки, включая слэши '/'
 
+Пример использования:
+path('articles/<int:year>/', views.year_archive)
+path('blog/<slug:post_slug>/', views.post_detail)
+
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('python_blog.urls')),  # Путь для главной страницы и маршрутов блога
-]
+    path('', main),
 
+    # Подключаем python_blog.urls
+    path('posts/', include('python_blog.urls')),
+]
