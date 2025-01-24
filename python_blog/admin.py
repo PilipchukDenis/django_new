899d8ced2ece01abb3eb7c073fd020447671d465
PilipchukDenis <grutden@mail.ru>
2025-01-24 11:42:16 +0300

    hw24

--- python_blog/admin.py
+++ python_blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Post, Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "updated_date")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
